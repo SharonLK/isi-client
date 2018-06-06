e29de8781f23e15cfa03d1e54113fd1368999c37
Sharon Levenzon Kuninin <sharonlevenzonkuninin@gmail.com>
2018-06-06 10:26:19 +0300

    Finish the UI of the Deploy New Function window

--- src/main/java/ui/DeployNewFunctionUI.kt
+++ src/main/java/ui/DeployNewFunctionUI.kt
@@ -1,13 +1,93 @@
 package ui
 
-import tornadofx.View
-import tornadofx.hbox
-import tornadofx.label
+import javafx.beans.property.SimpleStringProperty
+import javafx.geometry.Orientation
+import javafx.scene.layout.Priority
+import javafx.scene.text.FontWeight
+import javafx.stage.FileChooser
+import tornadofx.*
 
 class DeployNewFunctionUI : View() {
     override val root = hbox {
-        label("AAA")
-        label("BBB")
-        label("CCC")
+        val selectedFile = SimpleStringProperty()
+
+        title = "Deploy New Function"
+
+        gridpane {
+            hgap = 20.0
+            vgap = 10.0
+
+            label("Name:") {
+                gridpaneConstraints {
+                    columnRowIndex(0, 0)
+                }
+            }
+
+            textfield {
+                gridpaneConstraints {
+                    columnRowIndex(1, 0)
+                }
+            }
+
+            label("Replicas:") {
+                gridpaneConstraints {
+                    columnRowIndex(0, 1)
+                }
+            }
+
+            textfield {
+                gridpaneConstraints {
+                    columnRowIndex(1, 1)
+                }
+            }
+
+            button("Select file") {
+                action {
+                    val fileChooser = FileChooser()
+                    val file = fileChooser.showOpenDialog(null)
+
+                    if (file != null) {
+                        selectedFile.set(file.absolutePath)
+                    }
+                }
+
+                gridpaneConstraints {
+                    columnRowIndex(0, 2)
+                }
+            }
+
+            label("File:") {
+                textProperty().bind(selectedFile)
+
+                gridpaneConstraints {
+                    columnRowIndex(1, 2)
+                }
+            }
+
+            separator(Orientation.HORIZONTAL) {
+                gridpaneConstraints {
+                    columnRowIndex(0, 3)
+                    columnSpan = 2
+                }
+            }
+
+            button("Deploy") {
+                hgrow = Priority.ALWAYS
+                maxWidth = Double.MAX_VALUE
+                prefHeight = 60.0
+
+                style {
+                    fontSize = Dimension(1.5, Dimension.LinearUnits.em)
+                    backgroundColor += c("#79B0C0")
+                    textFill = c("#FFFFFF")
+                    fontWeight = FontWeight.BOLD
+                }
+
+                gridpaneConstraints {
+                    columnRowIndex(0, 4)
+                    columnSpan = 2
+                }
+            }
+        }
     }
 }
